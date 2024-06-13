0ab897d6b7a03d22db0b17626ed6d4381480a0f5
Viktor Tiahur <abik2002@ukr.net>
2024-06-13 18:33:53 +0200

    13.06_build

--- app.js
+++ app.js
@@ -1,2 +1,58 @@
 const resultElement = document.getElementById('result')
-console.log(resultElement.textContent);
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+const multiplyBtn = document.getElementById('multiply')
+const divideBtn = document.getElementById('divide')
+
+let action = '+'
+
+plusBtn.onclick = function(){
+    action = '+'
+}
+minusBtn.onclick = function(){
+    action = '-'
+}
+multiplyBtn.onclick = function(){
+    action = '*'
+}
+divideBtn.onclick = function(){
+    action = '/'
+}
+
+
+function colorResult (result){
+    if(result < 0){
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result
+}
+
+submitBtn.onclick = function(){
+    if (action == '-'){
+        const sum = Number(input1.value) - Number(input2.value)
+        colorResult(sum)
+    }
+
+    if (action == '+'){
+        const sum = Number(input1.value) + Number(input2.value)
+        colorResult(sum)
+
+    }
+
+    if (action == '*'){
+        const sum = Number(input1.value) * Number(input2.value)
+        colorResult(sum)
+
+    }
+    if (action == '/'){
+        const sum = Number(input1.value) / Number(input2.value)
+        colorResult(sum)
+
+    }
+     
+}
