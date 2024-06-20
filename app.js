30e969efddfe541c3e2c67f6a87f688ad6339e9c
Viktor Tiahur <abik2002@ukr.net>
2024-06-20 22:08:01 +0200

    Html finish

--- app.js
+++ app.js
@@ -1,58 +1,63 @@
-const resultElement = document.getElementById('result')
-const input1 = document.getElementById('input1')
-const input2 = document.getElementById('input2')
-const submitBtn = document.getElementById('submit')
-const plusBtn = document.getElementById('plus')
-const minusBtn = document.getElementById('minus')
-const multiplyBtn = document.getElementById('multiply')
-const divideBtn = document.getElementById('divide')
+// const resultElement = document.getElementById('result')
+// const input1 = document.getElementById('input1')
+// const input2 = document.getElementById('input2')
+// const submitBtn = document.getElementById('submit')
+// const plusBtn = document.getElementById('plus')
+// const minusBtn = document.getElementById('minus')
+// const multiplyBtn = document.getElementById('multiply')
+// const divideBtn = document.getElementById('divide')
 
-let action = '+'
+// let action = '+'
 
-plusBtn.onclick = function(){
-    action = '+'
-}
-minusBtn.onclick = function(){
-    action = '-'
-}
-multiplyBtn.onclick = function(){
-    action = '*'
-}
-divideBtn.onclick = function(){
-    action = '/'
-}
+// plusBtn.onclick = function(){
+//     action = '+'
+// }
+// minusBtn.onclick = function(){
+//     action = '-'
+// }
+// multiplyBtn.onclick = function(){
+//     action = '*'
+// }
+// divideBtn.onclick = function(){
+//     action = '/'
+// }
 
 
-function colorResult (result){
-    if(result < 0){
-        resultElement.style.color = 'red'
-    } else {
-        resultElement.style.color = 'green'
-    }
-    resultElement.textContent = result
-}
+// function colorResult (result){
+//     if(result < 0){
+//         resultElement.style.color = 'red'
+//     } else {
+//         resultElement.style.color = 'green'
+//     }
+//     resultElement.textContent = result
+// }
 
-submitBtn.onclick = function(){
-    if (action == '-'){
-        const sum = Number(input1.value) - Number(input2.value)
-        colorResult(sum)
-    }
+// submitBtn.onclick = function(){
+//     if (action == '-'){
+//         const sum = Number(input1.value) - Number(input2.value)
+//         colorResult(sum)
+//     }
 
-    if (action == '+'){
-        const sum = Number(input1.value) + Number(input2.value)
-        colorResult(sum)
+//     if (action == '+'){
+//         const sum = Number(input1.value) + Number(input2.value)
+//         colorResult(sum)
 
-    }
+//     }
 
-    if (action == '*'){
-        const sum = Number(input1.value) * Number(input2.value)
-        colorResult(sum)
+//     if (action == '*'){
+//         const sum = Number(input1.value) * Number(input2.value)
+//         colorResult(sum)
 
-    }
-    if (action == '/'){
-        const sum = Number(input1.value) / Number(input2.value)
-        colorResult(sum)
+//     }
+//     if (action == '/'){
+//         const sum = Number(input1.value) / Number(input2.value)
+//         colorResult(sum)
 
-    }
+//     }
      
-}
+// }
+
+let btn = document.getElementById('multiply')
+btn.addEventListener("click", function() {
+    this.classList.add("active");
+  });
